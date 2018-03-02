a602358d6619b4cde8c1a39a70c2985efc9795f7
Erdenesukh Tsendjav <32915874+sukh0128@users.noreply.github.com>
2018-03-01 21:52:28 -0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 2;
+
+        
+
+            for (int i = 0; i < N; i++)
+            {
+                string word = Console.ReadLine();
+                List<char> word_list = word.ToList();
+                List<string> odds = new List<string>();
+                List<string> evens = new List<string>();
+                int word_size = word.Length;
+
+                for (int j = 0; j < word_size; j++)
+                {
+
+                    Console.Write(j);
+                    if (j % 2 == 0)
+                    {
+                        evens.Add(char.ToString(word_list[j]));
+                        Console.WriteLine("gaga");
+                    }
+                    else if (j % 2 != 0)
+                    {
+
+                        odds.Add(char.ToString(word_list[j]));
+                        Console.WriteLine("dada");
+                    }
+                }
+               
+                for (int a = 0; a < evens.Count; a++)
+                {
+                    Console.Write(evens[a]);
+
+                }
+                Console.Write("");
+                for (int a = 0; a < odds.Count; a++)
+                {
+                    Console.Write(odds[a]);
+                }
+                Console.ReadLine();
+            }
+            }
+        }
+    }
+
